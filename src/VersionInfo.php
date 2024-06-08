dbf95fa0d80cc79c4fd6cb48923049fceb340d2a
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-08 21:27:03 +0700

    Update VersionInfo.php

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,7 +31,7 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "XPocketMP";
-	public const BASE_VERSION = "10.01";
+	public const BASE_VERSION = "10.0.0";
 	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_CHANNEL = "main";
 
