c0cca7b83ad873d03a6aaa5a3340834e331737e3
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-11 21:24:41 +0700

    Add files via upload

--- src/entity/Villager.php
+++ src/entity/Villager.php
@@ -1,99 +1,96 @@
 <?php
 
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+ */
+
 declare(strict_types=1);
 
 namespace pocketmine\entity;
 
-use pocketmine\item\ItemFactory;
-use pocketmine\item\VanillaItems;
-use pocketmine\math\Vector3;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\types\entity\EntityIds;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataCollection;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataFlags;
 use pocketmine\network\mcpe\protocol\types\entity\EntityMetadataProperties;
-use pocketmine\world\BlockTransaction;
-use pocketmine\block\BlockLegacyIds;
 
-class Villager extends Living implements Ageable {
-    public const PROFESSION_FARMER = 0;
-    public const PROFESSION_LIBRARIAN = 1;
-    public const PROFESSION_PRIEST = 2;
-    public const PROFESSION_BLACKSMITH = 3;
-    public const PROFESSION_BUTCHER = 4;
+class Villager extends Living implements Ageable{
+	public const PROFESSION_FARMER = 0;
+	public const PROFESSION_LIBRARIAN = 1;
+	public const PROFESSION_PRIEST = 2;
+	public const PROFESSION_BLACKSMITH = 3;
+	public const PROFESSION_BUTCHER = 4;
 
-    private const TAG_PROFESSION = "Profession"; //TAG_Int
+	private const TAG_PROFESSION = "Profession"; //TAG_Int
 
-    public static function getNetworkTypeId() : string {
-        return EntityIds::VILLAGER;
-    }
+	public static function getNetworkTypeId() : string{ return EntityIds::VILLAGER; }
 
-    private bool $baby = false;
-    private int $profession = self::PROFESSION_FARMER;
+	private bool $baby = false;
+	private int $profession = self::PROFESSION_FARMER;
 
-    protected function getInitialSizeInfo() : EntitySizeInfo {
-        return new EntitySizeInfo(1.8, 0.6); // Sesuaikan dengan tinggi mata jika diperlukan
-    }
+	protected function getInitialSizeInfo() : EntitySizeInfo{
+		return new EntitySizeInfo(1.8, 0.6); //TODO: eye height??
+	}
 
-    public function getName() : string {
-        return "Villager";
-    }
+	public function getName() : string{
+		return "Villager";
+	}
 
-    protected function initEntity(CompoundTag $nbt) : void {
-        parent::initEntity($nbt);
-        $this->setProfession($nbt->getInt(self::TAG_PROFESSION, self::PROFESSION_FARMER));
-    }
+	protected function initEntity(CompoundTag $nbt) : void{
+		parent::initEntity($nbt);
 
-    public function saveNBT() : CompoundTag {
-        $nbt = parent::saveNBT();
-        $nbt->setInt(self::TAG_PROFESSION, $this->getProfession());
-        return $nbt;
-    }
+		/** @var int $profession */
+		$profession = $nbt->getInt(self::TAG_PROFESSION, self::PROFESSION_FARMER);
 
-    public function setProfession(int $profession) : void {
-        if ($profession < self::PROFESSION_FARMER || $profession > self::PROFESSION_BUTCHER) {
-            $profession = self::PROFESSION_FARMER;
-        }
-        $this->profession = $profession;
-        $this->networkPropertiesDirty = true;
-    }
+		if($profession > 4 || $profession < 0){
+			$profession = self::PROFESSION_FARMER;
+		}
 
-    public function getProfession() : int {
-        return $this->profession;
-    }
+		$this->setProfession($profession);
+	}
 
-    public function isBaby() : bool {
-        return $this->baby;
-    }
+	public function saveNBT() : CompoundTag{
+		$nbt = parent::saveNBT();
+		$nbt->setInt(self::TAG_PROFESSION, $this->getProfession());
 
-    protected function syncNetworkData(EntityMetadataCollection $properties) : void {
-        parent::syncNetworkData($properties);
-        $properties->setGenericFlag(EntityMetadataFlags::BABY, $this->baby);
-        $properties->setInt(EntityMetadataProperties::VARIANT, $this->profession);
-    }
+		return $nbt;
+	}
 
-    public function onUpdate(int $currentTick) : bool {
-        if($this->isClosed()){
-            return false;
-        }
+	/**
+	 * Sets the villager profession
+	 */
+	public function setProfession(int $profession) : void{
+		$this->profession = $profession; //TODO: validation
+		$this->networkPropertiesDirty = true;
+	}
 
-        if($this->profession === self::PROFESSION_FARMER){
-            $this->performFarming();
-        }
+	public function getProfession() : int{
+		return $this->profession;
+	}
 
-        return parent::onUpdate($currentTick);
-    }
+	public function isBaby() : bool{
+		return $this->baby;
+	}
 
-    private function performFarming() : void {
-        $randomX = mt_rand(-5, 5);
-        $randomZ = mt_rand(-5, 5);
-        $position = $this->getPosition()->add($randomX, 0, $randomZ);
-        $block = $this->getWorld()->getBlock($position);
+	protected function syncNetworkData(EntityMetadataCollection $properties) : void{
+		parent::syncNetworkData($properties);
+		$properties->setGenericFlag(EntityMetadataFlags::BABY, $this->baby);
 
-        if ($block->getId() === BlockLegacyIds::DIRT || $block->getId() === BlockLegacyIds::GRASS) {
-            $this->getWorld()->setBlock($position, BlockFactory::getInstance()->get(BlockLegacyIds::FARMLAND, 0));
-            $seed = ItemFactory::getInstance()->get(ItemIds::WHEAT_SEEDS);
-            $this->getWorld()->setBlock($position->up(), $seed->getBlock());
-        }
-    }
+		$properties->setInt(EntityMetadataProperties::VARIANT, $this->profession);
+	}
 }
